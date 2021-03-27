ddba0115bc4dd36852cb37b7c90524f2b0a62cb6
Franco <francofwolff@gmail.com>
2021-03-27 15:35:20 -0300

    2nd commit, css cleaning and stuff

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,12 @@
 
 //SWIPER
 
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-banner', {
     cssMode: true,
     spaceBetween: 30,
     centeredSlides: true,
-    mousewheel: true,
-    keyboard: true,
+    keyboard: false,
+    mousewheel: false,
     autoplay: {
         delay: 5000,
         disableOnInteraction: false,
@@ -24,3 +24,26 @@ var swiper = new Swiper('.swiper-container', {
   });
 
 //
+
+//SWIPER MINI
+
+var swipermini = new Swiper('.mini-swiper', {
+  cssMode: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  mousewheel: false,
+  keyboard: false,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swipermini-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swipermini-pagination',
+    clickable: true,
+  },
+  
+});
