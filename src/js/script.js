3e0063302a251a33bc7fc9526147812f2c984136
Franco <francofwolff@gmail.com>
2021-03-28 13:35:37 -0300

    Mini Swipper Settings

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,6 @@ var swiper = new Swiper('.swiper-banner', {
     cssMode: true,
     spaceBetween: 30,
     centeredSlides: true,
-    keyboard: false,
-    mousewheel: false,
     autoplay: {
         delay: 5000,
         disableOnInteraction: false,
@@ -31,18 +29,16 @@ var swipermini = new Swiper('.mini-swiper', {
   cssMode: true,
   spaceBetween: 30,
   centeredSlides: true,
-  mousewheel: false,
-  keyboard: false,
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
   },
   navigation: {
-    nextEl: '.swipermini-button-next',
+    nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   pagination: {
-    el: '.swipermini-pagination',
+    el: '.swiper-pagination',
     clickable: true,
   },
   
